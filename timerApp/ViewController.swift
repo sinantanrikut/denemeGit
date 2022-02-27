2d39bc64a41bad1fd2485f2f0b32bf3749e78497
Sinan Tanrıkut <sinantanrikut7@gmail.com>
2022-02-27 05:53:52 +0300

    Initial Commit

---
+++ timerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  timerApp
+//
+//  Created by Sinan Tanrıkut on 27.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
